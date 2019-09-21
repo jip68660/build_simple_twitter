7e14f785f5fb1da407583912588ed6109600ab3b
Hyunchel Kim <hyunchel.inbox@gmail.com>
2019-09-21 14:59:38 -0500

    Add login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,42 +2,46 @@ import React from 'react';
 import InputFormat from './InputFormat';
 import ButtonFormat from './ButtonFormat';
 
-class Signup extends React.Component {
+class Login extends React.Component {
+
     constructor(props) {
         super(props);
 
-        this.handleName = this.handleName.bind(this);
         this.handleHandle = this.handleHandle.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
     }
-    handleName(event) {
-        this.props.handleChange("name", event);
-    }
+
     handleHandle(event) {
         this.props.handleChange("handle", event);
     }
+
+    handlePassword(event) {
+        this.props.handleChange("password", event);
+    }
+
     render() {
         return(
             <div className="inputTemplate">
                 <div className="topContent">
-                    <InputFormat 
-                        className={ "getName" } 
-                        placeholder={ "Name" } 
-                        value={ this.props.name } 
-                        name={ "name" } 
-                        handleChange={ this.handleName }
-                    />
                     <InputFormat
                         className={ "getHandle" }
                         placeholder={ "Handle" }
                         value={ this.props.handle }
-                        name={ "handle" }
+                        name={ "userHandle" }
                         handleChange={ this.handleHandle }
+                    />
+                    <InputFormat 
+                        className={ "getPassword" } 
+                        placeholder={ "Password" } 
+                        value={ this.props.password } 
+                        name={ "userPassword" } 
+                        handleChange={ this.handlePassword }
                     />
                 </div>
                 <ButtonFormat 
                     className={ "submitButton save" } 
-                    handleSubmit={ this.props.handleSubmit } 
-                    buttonValue={ "Save"}
+                    handleSubmit={ this.props.handleLogin } 
+                    buttonValue={ "Login" }
                 /> 
             </div>
         );
@@ -45,4 +49,4 @@ class Signup extends React.Component {
     }
 }
 
-export default Signup;
+export default Login;
